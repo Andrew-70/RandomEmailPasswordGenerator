87c55e61cf5f6d390311a2070675933a3037eede
Andrew QA <flashyandrew78@gmail.com>
2023-08-15 00:08:56 -0400

    update 1

--- pythonProject/Flask/Flaskblog.py
+++ pythonProject/Flask/Flaskblog.py
@@ -1,13 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
-
+posts = [
+    {
+        'author': 'Bugie',
+        'title': 'web development with flask',
+        'content': 'using templates',
+        'date_posted': 'August 14, 2023'
+    },
+{
+        'author': 'Ros',
+        'title': 'test automation',
+        'content': 'using selenium',
+        'date_posted': 'August 14, 2023'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html')
